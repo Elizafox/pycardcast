ed95458205aba3713e55ae439e51f6866d5d6d93
Elizabeth Myers <elizabeth@interlinked.me>
2015-07-28 17:10:44 -0500

    Fix stupid mistake

--- pycardcast/util.py
+++ pycardcast/util.py
@@ -9,5 +9,5 @@ from datetime import datetime
 def isoformat(date):
     """Small, unpedantic ISO format parser (as used by Cardcast)."""
 
-    assert date.endswith("+00:00", "This cannot handle non-UTC offsets yet!")
+    assert date.endswith("+00:00"), "This cannot handle non-UTC offsets yet!"
     return datetime.strptime(date[:-6], "%Y-%m-%dT%H:%M:%S")
